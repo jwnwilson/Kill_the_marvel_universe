7d20a11c14ca1e1f7800d0ad8a70c687c36bed39
Noel Wilson <jwnwilson@hotmail.co.uk>
2017-04-08 21:34:09 +0100

    Finished the formatting and make the reporter structure nice before moving into the second part

--- data_handlers/reporter.py
+++ data_handlers/reporter.py
@@ -1,3 +1,5 @@
+from functools import reduce
+import operator
 import sys
 
 from .base import BaseDataHandler
@@ -23,18 +25,49 @@ class MarvelReporter(BaseDataHandler):
     def _get_character_list(self):
         return [self.api_data[x] for x in self.api_data]
 
-    def alphabetic_characters(self):
+    def _get_list_character_attr(self, characters, attr):
+        return [ch[attr] for ch in characters]
+
+    def _get_list_character_attrs(self, characters, attr_list):
+        character_list = []
+
+        for ch in characters:
+            char_dict = {}
+            for attr in attr_list:
+                sub_attrs = attr.split('__')
+                char_dict[attr] = reduce(operator.getitem, sub_attrs, ch)
+            character_list.append(char_dict)
+
+        return character_list
+
+    def _limit_list(self, data_list, limit):
+        if limit:
+            data_list = data_list[:limit]
+        return data_list
+
+    def _format_data_list(self, data_list):
+        formated_str = ''
+        for item in data_list:
+            formated_str += str(item) + '\n'
+
+        return formated_str
+
+    def alphabetic_characters(self, limit=None):
         characters = self._get_character_list()
         characters = sorted(characters, key=lambda x: x['name'].lower())
-        character_names = [ch['name'] for ch in characters]
+        names = self._get_list_character_attr(characters, 'name')
+        names = self._limit_list(names, limit)
 
-        sys.stdout.write(str(character_names))
+        sys.stdout.write(self._format_data_list(names))
 
     def most_popular_characters(self):
         characters = self._get_character_list()
-        characters = sorted(characters, key=lambda x: x['comics']['available'], reverse=True)
-        character_names = [ch['name'] for ch in characters]
+        characters = sorted(
+            characters, key=lambda x: x['comics']['available'], reverse=True)
+        characters = self._get_list_character_attrs(
+            characters, ['name', 'comics__available'])
+        characters = self._limit_list(characters, 10)
 
-        sys.stdout.write(str(character_names))
+        sys.stdout.write(self._format_data_list(characters))
 
 
