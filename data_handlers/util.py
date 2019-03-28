e7b1f344fafc910f6e9f147c0e8deb57c86a2907
Noel <noel.wilson@supercarers.com>
2019-03-28 22:38:11 +0000

    Fixing regex

--- data_handlers/util.py
+++ data_handlers/util.py
@@ -1,10 +1,8 @@
 import re
 
 def get_id_from_resource(resource_str):
-    import pdb;pdb.set_trace()
-    return re.find(r'\/\d+\/', url)
+    return re.findall(r'\/\d+\/', url)[0]
 
 
 def get_id_from_url(url):
-    import pdb;pdb.set_trace()
-    return re.find(r'\/\d+\/', url)
+    return re.findall(r'\/(\d+)\/', url)[0]
